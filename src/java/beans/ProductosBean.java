853ec1e6a3f2cb4be8e135b79e64b0610759ff3b
ferpm <ferpm@DESKTOP-MANRVUP>
2020-11-13 19:00:37 -0600

    Agregar productos

--- src/java/beans/ProductosBean.java
+++ src/java/beans/ProductosBean.java
@@ -1,10 +1,64 @@
 package beans;
 
+import bl.CrearArchivo;
+import bl.Img;
+import bl.Producto;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import org.primefaces.event.FileUploadEvent;
 
 @ManagedBean(name = "productosB") //Aqui va el nombre del Bean
 @ViewScoped // Aquí va el tipo de scope que quiere que tenga este controlador
+
 public class ProductosBean {
+    private int categoria = 0;
+    private Producto producto;
+    private Img imagen;
+    private CrearArchivo ca = new CrearArchivo();
+    
+    public void AgregarProducto(){
+        
+        ca.crearArchivoProductos(imagen,producto);
+    }
+    
+    public void obtenerImagenProducto(FileUploadEvent event) {
+        try {
+            imagen = new Img();
+            imagen.setImagen(event.getFile().getFileName());
+            imagen.setContenttype(event.getFile().getContentType());
+            imagen.setInputstream(event.getFile().getInputstream());
+            imagen.setTamano(event.getFile().getSize());
+
+            
+        } catch (Exception e) {
+            System.err.println("Error al obtener el archivo " + e.toString());
+        }
+    }
+
+    
+    public int getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(int categoria) {
+        this.categoria = categoria;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Img getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(Img imagen) {
+        this.imagen = imagen;
+    }
+    
     
 }
--- src/java/bl/CrearArchivo.java
+++ src/java/bl/CrearArchivo.java
@@ -43,7 +43,33 @@ public class CrearArchivo {
     }
 
     public void crearArchivoProductos(Img imagen, Producto idProducto) {
-
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(new File(Servicios.ruta + "/" + imagen.getImagen()));
+            int read;
+            byte[] bytes = new byte[1024];
+            while ((read = imagen.getInputstream().read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.toString());
+        } finally {
+            if (imagen.getInputstream() != null) {
+                try {
+                    imagen.getInputstream().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    // outputStream.flush();
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
