3d28311f49f47e726ba01aa4213910a45b76d169
ferpm <ferpm@192.168.1.2>
2021-02-27 22:09:07 -0600

    Login

---
+++ src/java/beans/loginBean.java
@@ -0,0 +1,52 @@
+package beans;
+
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Fernanda Porras
+ */
+
+@ManagedBean(name = "LoginB")
+@SessionScoped
+
+public class loginBean {
+
+    private String inputUser = "";
+    private String inputPassword = "";
+    private String user = "npcadmin";
+    private String password = "npcadmin";
+
+    public void validarlogin() throws IOException {
+        HttpSession httpSession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+
+        if (inputUser.equals(user) && inputPassword.equals(password)) {
+            FacesContext.getCurrentInstance().getExternalContext().redirect("indexAdministrador.xhtml");
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario o contraseña inválida", ""));
+        }
+
+    }
+
+    public String getInputUser() {
+        return inputUser;
+    }
+
+    public void setInputUser(String inputUser) {
+        this.inputUser = inputUser;
+    }
+
+    public String getInputPassword() {
+        return inputPassword;
+    }
+
+    public void setInputPassword(String inputPassword) {
+        this.inputPassword = inputPassword;
+    }
+
+}
