fc37a194ccc4b546a670e823fd07f1963e23fcbc
cexarz <52362128+cexarz@users.noreply.github.com>
2021-02-28 21:20:45 -0600

    Puras vistas

--- src/java/beans/VistaProductosBean.java
+++ src/java/beans/VistaProductosBean.java
@@ -25,6 +25,9 @@ public class VistaProductosBean {
     private List<Producto> productosImpresoras = new ArrayList<>();
     private List<Producto> productosEscritorio = new ArrayList<>();
     private List<Producto> productosPortatiles = new ArrayList<>();
+    private List<Producto> productosCCTV = new ArrayList<>();
+    private List<Producto> productosProtectores = new ArrayList<>();
+    private List<Producto> productosOficina = new ArrayList<>();
     private String nombreImagen = "";
 
     private Producto productoSeleccionado;
@@ -35,7 +38,9 @@ public class VistaProductosBean {
         productosImpresoras = Servicios.ObtenerProductosCateg(1);
         productosEscritorio = Servicios.ObtenerProductosCateg(2);
         productosPortatiles = Servicios.ObtenerProductosCateg(3);
-        
+        productosCCTV = Servicios.ObtenerProductosCateg(4);
+        productosProtectores = Servicios.ObtenerProductosCateg(5);
+        productosOficina = Servicios.ObtenerProductosCateg(6);
     }
     
     public StreamedContent getImageProductoImpresora() throws IOException {
@@ -49,6 +54,71 @@ public class VistaProductosBean {
             String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
             
             return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/Impresoras/"+idProducto+"/", filename)));
+        }
+    }
+        public StreamedContent getImageProductoPortatil() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String filename = context.getExternalContext().getRequestParameterMap().get("imagen");
+            String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
+            
+            return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/Portatiles/"+idProducto+"/", filename)));
+        }
+    }
+           public StreamedContent getImageProductoEscritorio() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String filename = context.getExternalContext().getRequestParameterMap().get("imagen");
+            String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
+            
+            return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/Escritorio/"+idProducto+"/", filename)));
+        }
+    }
+            public StreamedContent getImageProductoCCTV() throws IOException {
+           FacesContext context = FacesContext.getCurrentInstance();
+           if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+               // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+               return new DefaultStreamedContent();
+           } else {
+               // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+               String filename = context.getExternalContext().getRequestParameterMap().get("imagen");
+               String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
+
+               return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/CCTV/"+idProducto+"/", filename)));
+           }
+       }
+            public StreamedContent getImageProductoProtectores() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String filename = context.getExternalContext().getRequestParameterMap().get("imagen");
+            String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
+            
+            return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/Protectores/"+idProducto+"/", filename)));
+        }
+    }
+            public StreamedContent getImageProductoOficina() throws IOException {
+        FacesContext context = FacesContext.getCurrentInstance();
+        if (context.getCurrentPhaseId() == PhaseId.RENDER_RESPONSE) {
+            // So, we're rendering the view. Return a stub StreamedContent so that it will generate right URL.
+            return new DefaultStreamedContent();
+        } else {
+            // So, browser is requesting the image. Return a real StreamedContent with the image bytes.
+            String filename = context.getExternalContext().getRequestParameterMap().get("imagen");
+            String idProducto = context.getExternalContext().getRequestParameterMap().get("id");
+            
+            return new DefaultStreamedContent(new FileInputStream(new File("/C:/NPC/Productos/Oficina/"+idProducto+"/", filename)));
         }
     }
     public void agregarCarrito() throws Exception{
@@ -85,6 +155,27 @@ public class VistaProductosBean {
 
     public void setProductosPortatiles(List<Producto> productosPortatiles) {
         this.productosPortatiles = productosPortatiles;
+    }
+      public List<Producto> getProductosCCTV() {
+        return productosCCTV;
+    }
+
+    public void setProductosCCTV(List<Producto> productosCCTV) {
+        this.productosCCTV = productosCCTV;
+    }
+     public List<Producto> getProductosProtectores() {
+        return productosProtectores;
+    }
+
+    public void setProductosProtectores(List<Producto> productosProtectores) {
+        this.productosProtectores = productosProtectores;
+    }
+     public List<Producto> getProductosOficina() {
+        return productosOficina;
+    }
+
+    public void setProductosOficina(List<Producto> productosOficina) {
+        this.productosOficina = productosOficina;
     }
 
     public String getNombreImagen() {
